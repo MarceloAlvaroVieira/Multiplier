a8c0b51aba140eb5cfc57f3693b2c4947a8152d9
MarceloAlvaroVieira <marceloalvarovieira@gmail.com>
2023-01-30 17:42:04 -0300

    feat(Teste Estoque): Criado teste de PATCH no endpoint de Estoque

--- __tests__/unit/Estoque.test.js
+++ __tests__/unit/Estoque.test.js
@@ -5,8 +5,23 @@ const app = require('../../src/app');
 describe('Estoque', () => {
     it('Busca de Estoque por id', async () => {
         const response = await request(app)
-           .get('/estoques/1')
+           .get('/produtos/1/estoque')
 
          expect(response.status).toBe(200);
+    })
+
+    it('Altera um Estoque existente', async () => {
+        const response = await request(app)
+            .patch('/produtos/1/estoque')
+            .send({
+                idProduto: 1,
+                quantidade: 10,
+                reserva: 5,
+                status: 1
+            }, {
+                where: {id: 1},
+            })
+
+        expect(response.status).toBe(200);
     })
 })
