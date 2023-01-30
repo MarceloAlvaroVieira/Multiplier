bd608e77a47e997b40b45b9a65de93f744b277a1
MarceloAlvaroVieira <marceloalvarovieira@gmail.com>
2023-01-30 17:05:21 -0300

    feat(Teste Estoque): Removido teste de GET no endpoint de Estoque

--- __tests__/unit/Estoque.test.js
+++ __tests__/unit/Estoque.test.js
@@ -3,14 +3,6 @@ const app = require('../../src/app');
 
 
 describe('Estoque', () => {
-
-    it('Listagem de Estoques', async () => {
-        const response = await request(app)
-            .get('/estoques')
-
-        expect(response.status).toBe(200);
-    })
-
     it('Busca de Estoque por id', async () => {
         const response = await request(app)
            .get('/estoques/1')
