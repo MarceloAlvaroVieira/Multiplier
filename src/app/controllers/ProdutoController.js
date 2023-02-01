7a052cb27a48ce5fec44716113bbe22f4df76890
MarceloAlvaroVieira <marceloalvarovieira@gmail.com>
2023-02-01 12:06:28 -0300

    feat(Endpoit Produto): cirado endpoid GET por id de produto

--- src/app/controllers/ProdutoController.js
+++ src/app/controllers/ProdutoController.js
@@ -18,6 +18,19 @@ class ProdutoController{
             return res.status(422).send(error)
         }
     }
+
+    async findOne(req, res){
+        try{
+            const id = parseInt(req.params.id)
+            const produto = await Produto.findByPk(id)
+            if(!produto || produto === []){
+                return res.status(404).send('Produto não encontrado.')
+            }
+            res.status(200).send(produto)
+        }catch(error){
+            return res.status(500).send(error)
+        }
+    }
 }
 
 module.exports = new ProdutoController();
